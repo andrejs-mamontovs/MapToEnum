08a88a32d7b2f14573b8b401b49da767bfed9710
Andrey Mamontov <andrey.mamontov@ctco.lv>
2016-11-21 10:07:32 +0200

    Update unittest

--- Tests/MapToEnumTests/MapToEnumTests.swift
+++ Tests/MapToEnumTests/MapToEnumTests.swift
@@ -2,16 +2,26 @@ import XCTest
 @testable import MapToEnum
 
 class MapToEnumTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //XCTAssertEqual(MapToEnum().text, "Hello, World!")
-
+    func testMapClass() {
         let arr:[String:Any] = ["Id": 1, "Name": "Record #1"]
 
         let map = Map.link(arr, and: Field.self)
 
         let data = Item()
+        data.Id <- map[.Id]
+        data.Name <- map[.Name]
+
+        XCTAssertEqual(data.Id, 1)
+        XCTAssertEqual(data.Name, "Record #1")
+
+    }
+
+    func testMapStruct() {
+        let arr:[String:Any] = ["Id": 1, "Name": "Record #1"]
+
+        let map = Map.link(arr, and: Field.self)
+
+        var data = ItemStruct()
         data.Id <- map[.Id]
         data.Name <- map[.Name]
 
@@ -24,13 +34,18 @@ class MapToEnumTests: XCTestCase {
         var Name: String?
     }
 
+    public struct ItemStruct {
+        var Id: Int?
+        var Name: String?
+    }
+
     public enum Field : String {
         case Id, Name
     }
 
     static var allTests : [(String, (MapToEnumTests) -> () throws -> Void)] {
         return [
-            ("testExample", testExample),
+            ("testMapClass", testMapClass), ("testMapStruct", testMapStruct)
         ]
     }
 }
